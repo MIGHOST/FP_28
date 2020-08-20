afcf03b18ba22c93811d708b2159750f0bd7e35a
Mashina <mi.sidorenko@gmail.com>
2020-08-20 16:35:24 +0300

    merge_test

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
-import style from "./Login.module.css";
+import { Link } from "react-router-dom";
+import styles from "./Login.module.css";
 import { useMediaQuery } from "react-responsive";
 
 const Login = () => {
@@ -10,53 +11,69 @@ const Login = () => {
   });
 
   return (
-    <div className={style.container}>
-      <div className={style.wrapper}>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
         {/**----wrapper-left---- */}
         {!isTablet && (
-          <div className={style.wrapper_left}>
-            <div className={style.wrapper_phone}></div>
-            <h2 className={style.wrapper_left__title}>Finance App</h2>
+          <div className={styles.wrapper_left}>
+            <div className={styles.wrapper_phone}></div>
+            <h2 className={styles.wrapper_left__title}>Finance App</h2>
           </div>
         )}
 
         {/**----wrapper-right---- */}
-        <div className={style.wrapper_right}>
-          <div className={style.wrapper_login}>
-            <div className={style.wrapper_title}>
-              <div className={style.img_logo}></div>
-              <h2 className={style.title}>Wallet</h2>
+        <div className={styles.wrapper_right}>
+          <div className={styles.wrapper_login}>
+            <div className={styles.wrapper_title}>
+              <div className={styles.img_logo}></div>
+              <h2 className={styles.title}>Wallet</h2>
             </div>
             {/**----form---- */}
-            <form className={style.form}>
-              <div className={style.input_wrapper}>
-                <div className={`${style.icon} ${style.icon_email}`}></div>
+            <form className={styles.form}>
+              <div className={styles.input_wrapper}>
+                <div className={`${styles.icon} ${styles.icon_email}`}></div>
                 <input
-                  className={`${style.input} ${style.input_email}`}
+                  className={`${styles.input} ${styles.input_email}`}
                   placeholder="E-mail"
                 />
               </div>
 
-              <div className={style.input_wrapper}>
-                <div className={`${style.icon} ${style.icon_password}`}></div>
+              <div className={styles.input_wrapper}>
+                <div className={`${styles.icon} ${styles.icon_password}`}></div>
                 <input
-                  className={`${style.input} ${style.input_password}`}
+                  className={`${styles.input} ${styles.input_password}`}
                   placeholder="Пароль"
                 />
               </div>
 
-              <button type="submit" className={style.buttom}>
+              <button type="submit" className={styles.buttom}>
                 Войти
               </button>
             </form>
-            <a className={style.link_registation} href="#">
+            <Link
+              to="/registration"
+              className={styles.link_registation}
+              href="#"
+            >
               Регистрация
-            </a>
-          </div>
+            </Link>
 
-          {isTablet && (
-            <h2 className={style.wrapper_left__title}>Finance App</h2>
-          )}
+            <div className={styles.login}>
+              <h3 className={styles.social_title}>Войти c помощью</h3>
+              <div className={styles.social_box}>
+                <Link to="/google" className={styles}>
+                  <div className={styles.google_box}></div>
+                </Link>
+                <Link to="/facebook" className={styles}>
+                  <div className={styles.fb_box}></div>
+                </Link>
+              </div>
+            </div>
+
+            {isTablet && (
+              <h2 className={styles.wrapper_left__title}>Finance App</h2>
+            )}
+          </div>
         </div>
       </div>
     </div>
