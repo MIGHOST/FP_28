40f7b0f33f327cbb9a8039f08d3da32941637446
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-08-20 15:02:09 +0300

    navigation styles corrected

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import MainPage from "./Containers/MainPage/MainPage";
 const App = () => {
   return (
     <>
       <h1>Final project</h1>
+      <MainPage />
     </>
   );
 };
---
+++ src/Containers/MainPage/MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Navigation from "../../Components/Navigation/Navigation";
+import TableTransaction from "../../Components/TableTransaction/TableTransaction";
+
+const MainPage = () => {
+  return (
+    <div>
+      <Navigation />
+      {/* <TableTransaction /> */}
+    </div>
+  );
+};
+
+export default MainPage;
