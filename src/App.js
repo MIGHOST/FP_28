368142936609d582e7a7ac95a556d8dc4fed9f9f
Dmytro K <dmytrokzmnk@gmail.com>
2020-08-22 17:19:44 +0300

    deleted the rubbish app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./fonts/HelveticaNeueCyr/stylesheet.css";
-import S from "./Components/Statistic/Statistic";
+
 const App = () => {
   return <h1>Final project</h1>;
 };
