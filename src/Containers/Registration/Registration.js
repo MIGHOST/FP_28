630a23859c1adb028d9ef0c90c7c39cf92cf5097
Max <54718475+Fuhrer13@users.noreply.github.com>
2020-08-26 23:16:28 +0300

    fix svg

--- src/Containers/Registration/Registration.js
+++ src/Containers/Registration/Registration.js
@@ -48,6 +48,17 @@ const Registration = () => {
               onChange={inputHandler}
               name="email"
             />
+            <span className={styles.spanEmail}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+                <path d="M0 0h24v24H0z" fill="none" />
+              </svg>
+            </span>
           </div>
           <div className={styles.formGroup}>
             <input
@@ -58,6 +69,17 @@ const Registration = () => {
               onChange={inputHandler}
               name="password"
             />
+            <span className={styles.spanPassword}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z" />
+              </svg>
+            </span>
           </div>
           <div className={styles.formGroup}>
             <input
@@ -68,6 +90,17 @@ const Registration = () => {
               onChange={inputHandler}
               name="confirmPassword"
             />
+            <span className={styles.spanConfirmPassword}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z" />
+              </svg>
+            </span>
             {form.confirmPassword && form.confirmPassword === form.password ? (
               <div className={styles.confirmed}></div>
             ) : (
@@ -83,6 +116,17 @@ const Registration = () => {
               onChange={inputHandler}
               name="name"
             />
+            <span className={styles.spanName}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M3 5v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.11 0-2 .9-2 2zm12 4c0 1.66-1.34 3-3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6v-1z" />
+                <path d="M0 0h24v24H0z" fill="none" />
+              </svg>
+            </span>
           </div>
 
           <button type="submit" className={styles.button}>
