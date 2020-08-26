134705b3b083bc062f5c4897f13044521fc170b2
Alexey <valnistuy@gmail.com>
2020-08-26 23:59:37 +0300

    :refactor: fixed error after request getUser when component App has mounting

--- src/redux/operations/statistics.js
+++ src/redux/operations/statistics.js
@@ -1,16 +1,8 @@
 import { getStatisticsItem, setAuthToken } from "../../api/walletServices";
 import { statisticGetData } from "../actions/statistic";
-import { filteredToken } from "../../helpers/convertator";
-import { getFromLocaleStorage } from "../../helpers/storage";
 import { loaderOn, loaderOff } from "../actions/loader";
 
-const token = filteredToken(
-  getFromLocaleStorage("persist:auth-token")
-    ? getFromLocaleStorage("persist:auth-token").token
-    : ""
-);
-
-export const statisticUserGet = (params) => async (dispatch) => {
+export const statisticUserGet = (params, token) => async (dispatch) => {
   dispatch(loaderOn());
   try {
     setAuthToken(token);
